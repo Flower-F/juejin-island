1e768c26cada3f949064366ed2a160c48a804ab3
FlowerF <1647231392@qq.com>
2022-12-14 15:23:08 +0800

    fix: type errors

--- src/shared/types/types.d.ts
+++ src/shared/types/types.d.ts
@@ -1,5 +1,11 @@
 declare module 'island:site-data' {
   import { UserConfig } from 'shared/types';
-  let siteData: UserConfig;
+  const siteData: UserConfig;
   export default siteData;
+}
+
+declare module 'island:routes' {
+  import { RouteObject } from 'react-router-dom';
+  const routes: RouteObject[];
+  export { routes };
 }
